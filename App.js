6bb7e2092a5a281a56025c01314a467dde9a1a81
liyaSileshi <liyasileshi@gmail.com>
2020-09-13 15:53:09 -0700

    question screen

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import SitesScreen from './components/SitesScreen'
 import QuestionScreen from './components/QuestionScreen'
 import AnswerScreen from './components/AnswerScreen'
 
-
 const Stack = createStackNavigator();
 export default function App() {
   return (
@@ -57,52 +56,3 @@ const styles = StyleSheet.create({
     borderColor: 'green'
   },
 });
-
-
-
-
-// import React, { Component } from 'react';
-// import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-// export default class App extends Component {
-// 	state = {
-// 		location: null
-// 	};
-
-// 	findCoordinates = () => {
-// 		navigator.geolocation.getCurrentPosition(
-// 			position => {
-// 				const location = JSON.stringify(position);
-
-// 				this.setState({ location });
-// 			},
-// 			error => Alert.alert(error.message),
-// 			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-// 		);
-// 	};
-
-// 	render() {
-// 		return (
-// 			<View style={styles.container}>
-// 				<TouchableOpacity onPress={this.findCoordinates}>
-// 					<Text style={styles.welcome}>Find My Coords?</Text>
-// 					<Text>Location: {this.state.location}</Text>
-// 				</TouchableOpacity>
-// 			</View>
-// 		);
-// 	}
-// }
-
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		justifyContent: 'center',
-// 		alignItems: 'center',
-// 		backgroundColor: '#F5FCFF'
-// 	},
-// 	welcome: {
-// 		fontSize: 20,
-// 		textAlign: 'center',
-// 		margin: 10
-//   }
-// })
--- components/QuestionScreen.js
+++ components/QuestionScreen.js
@@ -2,13 +2,16 @@ import { StyleSheet, Text, Button, SafeAreaView,TouchableHighlight, ScrollView  
 import React, {useState, useEffect, useMemo} from 'react';
 import { CheckBox } from 'react-native-elements'
 
-// currently displays title, description and image on the question screen
+/*
+  Question Screen: Displays a question for the selected location
+*/
+
 export default function QuestionScreen({ route, navigation }) {
-  const [check, setCheck] = useState(1); 
-  let [shuffleChoices, setChoices] = useState([]);
+  const [check, setCheck] = useState(1); //used to check only 1 answer (like radio button)
+  let [shuffleChoices, setChoices] = useState([]); //to shuffle the choice for the selected location (...needs work)
   const {title, description, parsed, places_id} = route.params //parameter passed down from sites screen
-  //loop over data description and make checkboxes
-  
+
+  //loop over each location description and make checkboxes
   let choices = parsed.map((place, index) => {
     const title = place.query.pages[places_id[index]].title
     const desc = place.query.pages[places_id[index]].extract
@@ -16,7 +19,7 @@ export default function QuestionScreen({ route, navigation }) {
     //if the description is only one sentence, 
     question === '' ? question = desc.split(/[\.\!]+(?!\d)\s*|\n+\s*/)[0] : desc.split(/[\.\!]+(?!\d)\s*|\n+\s*/)[1];
 
-    //search the title in the question, and replace it with an empty string
+    //search the title in the description, and replace it with an empty string
     question = question.replace(title, '')
 
     return(
@@ -46,21 +49,21 @@ export default function QuestionScreen({ route, navigation }) {
   // }, [])
   
   return (
-      <SafeAreaView style={styles.question}>
-          <Text style={styles.placename}>You have chosen {title}!</Text>
-          <Text style={styles.quest}>Q: What is this site known for?</Text>
-          <ScrollView>
-            {choices}
-          </ScrollView>
-          <TouchableHighlight
-            style={styles.submitBtn}
-            onPress= {() => navigation.navigate('Answer', {answer: description === check, desc: description})}
-          >
-            <Text style={styles.submitTxt}>
-              Submit
-            </Text>
-          </TouchableHighlight>
-      </SafeAreaView>
+    <SafeAreaView style={styles.question}>
+      <Text style={styles.placename}>You have chosen {title}!</Text>
+      <Text style={styles.quest}>Q: What is this site known for?</Text>
+      <ScrollView>
+        {choices}
+      </ScrollView>
+      <TouchableHighlight
+        style={styles.submitBtn}
+        onPress= {() => navigation.navigate('Answer', {answer: description === check, desc: description})}
+      >
+        <Text style={styles.submitTxt}>
+          Submit
+        </Text>
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
